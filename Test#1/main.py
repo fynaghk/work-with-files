cb6fa100fa0fbb1abee0764ec6a02f89167faad4
Nağıyeva Fidan <55505161+fynaghk@users.noreply.github.com>
2020-09-06 09:52:12 +0400

    Update main.py

--- Test#1/main.py
+++ Test#1/main.py
@@ -1,9 +1,18 @@
+import os
+
+remove_ = False
 with open("group info.txt", "a") as file:
     size = int(input("Enter number "))
     for n in range(size):
         person_ = input(f"Enter {n + 1}. person: ")
         file.write(person_ + "\n")
         print()
-with open("group info.txt", "r") as file:
-    for _ in range(size):
-        print(file.read())
+
+enter = input("Do you want to delete this file? ")
+if (enter.lower() == "yes"):
+    os.remove("group info.txt")
+    print("You deleted file!")
+else:
+    with open("group info.txt", "r") as file:
+        for _ in range(size):
+            print(file.read())
